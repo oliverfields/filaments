a2aa2ef09125055487b80c3bd546d908aa348dc6
Oliver Fields <oliver@phnd.net>
2022-06-20 19:21:36 +0000

    Goodbye dropbox, hello diy linux scripting

---
+++ systemd/content_watch_deployer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Watch content directory for a deploy file, if found deploy site and remove deploy file.
+
+
+
+date >> "/tmp/content_watcher"
+
+PAGEGEN_DIR="/home/oliver/Documents/Projects/filaments"
+CONTENT_DIR="$PAGEGEN_DIR/content"
+
+
+inotifywait -m "$CONTENT_DIR" -e create |
+	while read directory action file; do
+		if [ "$file" = "deploy" ]; then
+			sudo -u oliver bash <<EOF
+cd "$PAGEGEN_DIR"
+pagegen -g prod
+rm "$CONTENT_DIR/deploy"
+EOF
+		fi
+	done
